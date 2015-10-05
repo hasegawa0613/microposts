d97c921f5462bd471933a34b9d76abf221f506d2
hasegawa0613 <hasegawa@techfactory.jp>
2015-10-05 05:41:24 +0000

    update profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get 'static_pages/home'
   #root to: 'static_pages#home'
