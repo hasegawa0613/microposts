eb1de0f9e185e74bb0693ccd61dae979fb8f3093
hasegawa0613 <hasegawa@techfactory.jp>
2015-10-17 10:42:40 +0000

    user form validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ActiveRecord::Base
     
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
+    validates :profile, presence: true, length: { maximum: 50 }
+    validates :area, presence: true, length: { maximum: 10 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
+    
+    
     has_many :following_relationships, class_name:  "Relationship",
                                      foreign_key: "follower_id",
                                      dependent:   :destroy
